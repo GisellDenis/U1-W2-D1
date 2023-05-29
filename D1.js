fde351496ddb406ea7a234bfa3563138787338f8
GisyWolf96 <gisymoon@gmail.com>
2023-05-29 15:27:13 +0200

    fine

--- D1.js
+++ D1.js
@@ -12,29 +12,41 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+let n1 = 10; // Questa è una variabile con valore numerico, che può essere sia un numero intero(cioe senza punto), una variabile a valore numerico decimele ( cioè un numero con un ponto come 10.25)
+let n2 = "Gabriela"; // questa è una variabile von valore stringa (cioè ha del testo al suo interno)
+let n4 = true; // Questa è una variabile che contiene un valore booleano, che può avere valore sia true(vero), che false (falso)
+let n6 = null; // Questo è un codice che viene inserito quando il valore della variabile è vuoto
+let n7 = undefined; // Questo è un codice che viene inserito quando il valore della variabile non è ancora stato assegnato
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+let name = "Gisell"
+console.log("il mio nome è",name)
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let a = 12
+let b = 20
+let c= a + b
+console.log("la somma di a+b è", c)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
+console.log(x)
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
+name = "Denis"
+console.log("il mio cognome è",name)
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -43,6 +55,8 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+x -= 4
+console.log("la sottrazione tra x e 4 è", x)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +65,6 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john"
+let name2 = "John"
+console.log(name1 === name2)
